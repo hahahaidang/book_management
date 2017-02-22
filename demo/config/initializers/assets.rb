366411815617094efc8b58a5ce3d9aaa7eea6ee4
Luong NgocHuyThanh <luong_ngoc_huy_thanh@ca-adv.co.jp>
2017-02-22 15:08:39 +0700

    complete_front_end_index_login_page

--- demo/config/initializers/assets.rb
+++ demo/config/initializers/assets.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -9,3 +8,14 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( login/login_page.css )
+Rails.application.config.assets.precompile += %w( welcome/index.css )
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.coffee )
+
+
+
+
+
+
+
