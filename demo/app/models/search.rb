c60a5de54bc35a0e1f70cc7b4f7400ef730f0683
Luong NgocHuyThanh <luong_ngoc_huy_thanh@ca-adv.co.jp>
2017-03-07 07:47:56 +0700

    7-3-2016-search-function-complete-fix-template

--- demo/app/models/search.rb
+++ demo/app/models/search.rb
@@ -1,6 +1,6 @@
 class Search < ActiveRecord::Base
   def self.func_search(para)
-    sql = "select r.quantity, r.status, r.created_at, u.`user_name`, b.`book_name`
+    sql = "select r.quantity, r.status, r.created_at, u.user_name, b.book_name
           from requests r, books b, users u
           where b.book_name like '#{para}' AND r.book_id=b.id and r.user_id = u.id"
     return ActiveRecord::Base.connection.exec_query(sql)
